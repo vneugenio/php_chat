f6e2d9460f07ab14982ce44b5df80ec369e4c3f2
bolts <voltaire.eugenio@gmail.com>
2018-09-12 03:29:10 +0800

    adding db connection and ui

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+    $host = 'localhost';
+    $username = 'root';
+    $password = '123';
+    $dbName = 'chat';
+
+    $connection = new mysqli($host, $username, $password, $dbName);
+?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    include 'db.php';
+
+    $query = 'SELECT * FROM chat ORDER BY id DESC';
+    $result = $connection->query($query);
+    $rows = $result->num_rows;
+    $data = $result->fetch_all();
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -9,21 +18,48 @@
         <div id="container">
             <div id="chat_box">
                 <div id="chat_data">
-                    <div class="message_details">
-                        <span class="name">Bolts: </span>
-                        <span class="message">Hello World!</span>
-                        <span class="timestamp">12:30am</span>
-                    </div>
+                    <?php
+                        for($i = 0; $i < $rows; $i++){
+
+                    ?>
+                        <div class="message_details">
+                            <span class="name">
+                                <?php echo $data[$i][1] . ':'; ?>
+                            </span>
+                            <span class="message">
+                                <?php echo $data[$i][2]; ?>
+                            </span>
+                            <span class="timestamp">
+                                <?php echo $data[$i][3]; ?>
+                            </span>
+                        </div>
+
+                    <?php
+                        }
+                    ?>
                 </div>
                 <form action="index.php" method="post">
                     <input placeholder="Enter your name"
                             type="text" name="name">
 
-                    <textarea name="enter_message"
+                    <textarea name="message"
                               placeholder="Enter Your Message"></textarea>
 
-                    <button>Submit</button>
+                    <button type="submit" name="submit">Submit</button>
                 </form>
+                <?php
+                    $name = $_POST['name'];
+                    $message = $_POST['message'];
+
+                    if(isset($name) && strlen($name) > 0) {
+                        $query = "INSERT INTO chat (name, message) VALUES
+                        ('$name', '$message')";
+
+                        $result = $connection->query($query);
+                    }
+
+                    unset($_POST);
+                ?>
             </div>
         </div>
     </body>
